8370a260c96abf9f0df2c7caa3a82362a50faaf8
Young <2109190674@qq.com>
2022-10-05 22:33:22 +0800

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+<<<<<<< HEAD
+=======
+        System.out.println("Iam Yh");
+>>>>>>> d4ffb74 (Initial commit)
     }
 }
