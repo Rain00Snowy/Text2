89137727b02a1beb143935a7855c5ce0b1fa4166
LiXingyanJason <117295381+LiXingyanJason@users.noreply.github.com>
2022-11-03 00:14:47 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("asdasdsadasdsad");
         System.out.println("asdasdsadasdsadasd");
         System.out.println("Iam Yh2");
+        System.out.println("曲径 错综复杂, 难解的事物; 曲折; 难以摆脱的困难");
     }
 }
